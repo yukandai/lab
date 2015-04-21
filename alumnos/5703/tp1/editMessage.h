33fdaf42bb3bf30e0162543e3ffd8be717eb4572
German Moyano <c.germanmoyano@gmail.com>
2015-04-20 21:27:03 -0300

    tp1

---
+++ alumnos/5703/tp1/editMessage.h
@@ -0,0 +1,17 @@
+#ifndef _EDITMESSAGE_H_
+#define _EDITMESSAGE_H_
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+#include <ctype.h>
+#include <unistd.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <errno.h>
+	
+	char * replace_filter(char *message, char *filter, char *replace);
+  
+#endif
+
