c8553686fe1ebd941c043d922075c29e50bee9ed
German Moyano <c.germanmoyano@gmail.com>
2015-04-25 16:55:50 -0300

    tp1 terminado

--- alumnos/5703/tp/tp1/main.c
+++ alumnos/5703/tp/tp1/main.c
@@ -3,33 +3,32 @@
 int main(int argc, char **argv){
 
 	int option, fd, file = 0, i, m_long;
-	char buff[SIZE], buff2[SIZE];	
+	char buff[SIZE], buff2[SIZE], *noComan, *noArg;	
 
 	while ((option = getopt(argc, argv, "o:h")) >= 0 ){ //Manejo argumentos
 		switch (option){
             case 'o': //guardo mensaje en otro archivo
-				file= open( optarg , O_RDWR | O_CREAT, 0777);
-
-				if(file < 0 ){
+				file = open( optarg , O_RDWR | O_CREAT, 0777);
+				if( file < 0 ){
 					perror("open()");
 					return -1;
 				}
-
 				break;
 
 		    case 'h': //imprimo mensaje de ayuda
 		    	help();
-		    	return -1;
+		    	return 0;
 
 		    default: //mensaje de error si ingresa otro comando
-				printf("No se reconoce el comando, por favor ejecuta ./tp1 -h por ayuda\n");
-				return 0;
+				noComan = "No se reconoce el comando, por favor ejecuta ./tp1 -h por ayuda.\n";
+				write(1, noComan, strlen(noComan));
+				return -1;
 		}
 	}
 
 	if(argc < 2){
-		char *noArg = "Ingrese al menos una palabra para filtrar. \n";
-		write(1, noArg, strlen (noArg));
+		noArg = "Ingrese al menos una palabra para filtrar.\n";
+		write(1, noArg, strlen(noArg));
 		return -1;
 	}
 
@@ -38,10 +37,11 @@ int main(int argc, char **argv){
 		m_long=0;
 		
 		for(i = 1; i < argc; i++){
-			strcpy(buff2,replace_filter(buff2, argv[i] , ""));
+			strcpy(buff2, replace_filter(buff2, argv[i], ""));
 			m_long = m_long + strlen(argv[i]);
 		}
-
+	
+		//if (file) ? write(file, buff2 , fd-m_long) : write(1, buff2, fd-m_long);
 		if(file)
 			write(file, buff2 , fd-m_long);
 		else
