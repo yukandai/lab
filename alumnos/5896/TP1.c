48c4ec84c13786664b326fb379799f73d04cbfec
Agustin Gras <rdc.aga@gmail.com>
2015-04-26 23:33:24 -0300

    ultima version

--- alumnos/5896/TP1.c
+++ alumnos/5896/TP1.c
@@ -7,7 +7,7 @@
 #include <string.h>
 #include <ctype.h>
 #define SIZE 80
-int process(char *input, int op, char *nombre,int len);
+#include "process.h"
 int main(int argc, char **argv)
 {
 int nchars = 0;
@@ -40,50 +40,13 @@ extern int optind, opterr, optopt;
 				case 'h':
 			//	printf("estoy en h \n");
 				printf("Modo de uso:\n./TP1: (make test1)\n Muestra mensaje filtrado en pantalla.\n");
-				printf("./tp1 -o mensajefiltrado.txt: (make test2)\nGuarda mensaje filtrado en un archivo llamado mensajefiltrado.txt\n");
+				printf("./tp1 -o mensajefiltrado.txt: (make test2)\nGuarda mensaje filtrado en un archivo llamado mensajei-filtrado.txt\n");
 				break;
 
 				}
 
 							}
-		return 0;
+		//return 0;
 		}
 
-int process(char *input, int op, char *nombre, int len)
-{
-  int i;
-  int m;
-  char filtrado[1024];
-  char palabra[1024]="hola";
-  char *token;
-  int fd;
-  int leido=0;
-  memset(filtrado, 0, sizeof filtrado);
 
-  fd = open(nombre,O_RDWR|O_CREAT,0777);
-   token = strtok(input, " \n\t");
-   m = strcmp(token,palabra);
-   if (m==0) {
-              token="****";
-             }  
-   strcat(filtrado,token);
-   strcat(filtrado," ");
-    while( (token = strtok( NULL, " \n\t" )) != NULL ){
-                                       i = strcmp(token,palabra);
-                                       if (i==0){
-						token="****";
-						}
-					strcat(filtrado,token);
-					strcat(filtrado," ");
-					}
-
-    if(op==0){	//muestra por salida
-	write(STDOUT_FILENO, filtrado, strlen(filtrado));
-	}else{ //guarda en archivo
-	printf("el mensaje se ha filtrado a un archivo llamado %s \n", nombre);
-
-
-	write(fd, filtrado, strlen(filtrado)); 
-    }
-return 0;
-}
