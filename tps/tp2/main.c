ce742fe4aacc961366fa07770788c329e8ae3fd2
J. Federico Hernandez <fede.hernandez@gmail.com>
2015-05-27 09:29:58 -0300

    Minor fixes.

--- tps/tp2/main.c
+++ tps/tp2/main.c
@@ -4,6 +4,17 @@
 
 int main(int argc, char **argv)
 {
+    switch (fork()) {
+    case 0: // hijo        
+        return 0;      
+
+    case -1: // error
+        perror("fork(): ");
+        return 1;
+
+    default: // padre
+        break;
+    }
 
     return 0;
 }
