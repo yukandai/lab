ccbcc3b4cc4c8c686672a6e29a5d749785fdc10c
J. Federico Hernandez <fede.hernandez@gmail.com>
2015-05-28 16:38:26 -0300

    Minor fixes.

--- tps/tp1/sol.c
+++ tps/tp1/sol.c
@@ -1,5 +1,5 @@
 /**
- *
+ * tp2
  */
 #include <unistd.h>
 #include <stdio.h>
@@ -9,28 +9,28 @@
 
 #define SIZE 80
 
-int filter(char *input, char **words_list, int size);
-int word_in_list(char *word, char **words_list, int size);
+int filter(char *input, const char **words_list, int list_len);
+int word_in_list(const char *word, const char **words_list, int list_len);
 
 
-int main(int argc, char **argv)
+int main(int argc, char * const * argv)
 {
     char buff[SIZE];
     int nchars;
-    char *words[] = {"hola", "mono", "tarántula"};
-    int size = sizeof (words) / sizeof (*words);
+    const char *words[] = {"hola", "mono", "tarántula"};
+    int list_len = sizeof (words) / sizeof (*words);
 
     memset(buff, 0, sizeof buff);
 
     while ((nchars = read(STDIN_FILENO, buff, sizeof buff)) > 0) {
-        filter(buff, words, size);
+        filter(buff, words, list_len);
     }
 
     return 0;
 }
 
 
-int filter(char *input, char **words_list, int size)
+int filter(char *input, const char **words_list, int list_len)
 {
     char *word;
     char *delim = "\t\r\n ";
@@ -38,7 +38,7 @@ int filter(char *input, char **words_list, int size)
     for (word = strtok(input, delim); word != NULL; word = strtok(NULL, delim)) {
         write(STDOUT_FILENO, " ", 1);
 
-        if (word_in_list(word, words_list, size) == 0) {
+        if (word_in_list(word, words_list, list_len) == 0) {
             write(STDOUT_FILENO, word, strlen(word));
         } else {
             write(STDOUT_FILENO, "* ", 2);
@@ -49,7 +49,7 @@ int filter(char *input, char **words_list, int size)
 }
 
 
-int word_in_list(char *word, char **words_list, int size)
+int word_in_list(const char *word, const char **words_list, int list_len)
 {
     int i;
     char *delim = "¡!¿?.,;:-_";
@@ -60,7 +60,7 @@ int word_in_list(char *word, char **words_list, int size)
     strncpy(tmp, word, sizeof tmp);
     _word = strtok_r(tmp, delim, &token);
 
-    for (i = 0; i < size; i++) {
+    for (i = 0; i < list_len; i++) {
         if (strncmp(_word, words_list[i], strlen(words_list[i])) == 0) {
 #if DEBUG
             printf("[x] Filtered word: %s\n", _word);
