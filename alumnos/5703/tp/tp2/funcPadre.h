22233b23ec306cf59b98da42ef2a70aba8ccb7e8
German Moyano <c.germanmoyano@gmail.com>
2015-06-04 22:38:16 -0300

    tp2

---
+++ alumnos/5703/tp/tp2/funcPadre.h
@@ -0,0 +1,10 @@
+#ifndef _PADRE_H_
+	#define _PADRE_H_
+
+	#include <stdio.h>
+	#include <unistd.h>
+	#include <signal.h>
+
+	int flagPadre = 0;
+
+#endif
---
+++ alumnos/5703/tp/tp2/global.h
@@ -0,0 +1,8 @@
+#ifndef _GLOBAL_H_
+	#define _GLOBAL_H_
+
+	#define READ 0
+	#define WRITE 1
+	#define MAX 1000
+
+#endif
