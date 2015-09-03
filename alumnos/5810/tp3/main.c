4cd1f769d7f7bc4563fb88c7db4b0bd779e26a04
Francisco Revoredo <francisco.r.89@gmail.com>
2015-09-03 11:19:50 -0300

    Update main.c

--- alumnos/5810/tp3/main.c
+++ alumnos/5810/tp3/main.c
@@ -5,33 +5,32 @@
 #include <sys/mman.h>
 #include <semaphore.h>
 #include <errno.h>
-#include <sys/stat.h> 
+#include <sys/stat.h>
 #include <fcntl.h>
 #include <string.h>
 #include <sys/time.h>
 
-#include "main.h"
-#include "comparar.h"
+#include "funhijo.h"
 
 
 
 int main(int argc, char **argv) {
 
-    char * result = (char*) calloc(1000, sizeof (char));
-
-    char * strptr = (char*) calloc(1000, sizeof (char));
-
-    char * aux = (char*) calloc(1000, sizeof (char));
-
-    char * token = (char*) calloc(1000, sizeof (char));
-
-    char * cen1 = (char*) calloc(1000, sizeof (char));
+    pid_t hijo;
+    sem_t *sem_1, *sem_2;
+    char *mem;
+    int *membits;
+    char cen1[1000];
+    char buff[100];
+    int bits = 0;
+    int opt, countflag = 0;
+    int leido;
 
     extern char *optarg;
     extern int optind, opterr, optopt;
 
 
-    while ((opt = getopt(argc, argv, "p:h:c")) != -1) {
+    while ((opt = getopt(argc, argv, "p:h")) != -1) {
         switch (opt) {
             case 'p':
 
@@ -48,17 +47,17 @@ int main(int argc, char **argv) {
 
                 break;
 
-            case 'c':
+/*            case 'c':
                 sem_close(sem_1);
                 sem_close(sem_2);
                 sem_unlink("/sem_2");
                 sem_unlink("/sem_1");
 
-                break;
+                break; */
 
             default:
 
-                write(1, "Uso: ./tp3 -p [PALABRA] \n\n Censura la palabras indicada y las reemplaza por caracteres ? en el texto de entrada\n\n -s [NOMBRE DE ARCHIVO] Seleccionar el nombre del archivo fuente para censurar \n                        (Source), si se omite se lee el stdin. \n -f [NOMBRE DE ARCHIVO] Seleccionar el nombre del archivo destino para la cens\n                        ura, si se omite la salida sera por stdout. \n -2 [PALABRA]           Seleccionar una segunda palabra adicional para censurar. \n -3 [PALABRA]           Seleccionar una tercera palabra adicional para censurar. \n -h                     Ver este archivo de ayuda. \n\n Nota: Para usar el parametro -2 y -3 se necesita haber usado los parametros anteriores.\n\n", 696);
+                write(1, "Uso: ./tp3 -p [PALABRA] \n\n Censura la palabra indicada y las reemplaza por caracteres ? en el texto de entrada\n\n El programa lee la entrada estándar (stdin) por lo que se puede utilizar pipes, \n al estilo 'cat texto.txt | ./tp3 -p palabra'. \n\n Si no se da la palabra a censurar el programa no se ejecuta. \n\n --\n", 313);
 
                 break;
         }
@@ -100,35 +99,12 @@ int main(int argc, char **argv) {
                 printf("\nerror creando el hijo\n");
                 exit(0);
             case 0: /* En el hijo */
-                printf("\nSoy el hijo con PID:%d\n\n", getpid());
-
+                
                 sem_wait(sem_2); /* espera que el padre libere el semáforo */
-
-                printf("\nSoy el hijo y empiezo a leer la memoria compartida.....\n");
-
-                bits = *membits;
-
-                strncpy(aux, mem, bits);
-
-                for (token = strtok(aux, delim); token; token = strtok(NULL, delim)) {
-
-                    result = comparar(token, cen1);
-                    strncat(strptr, result, strlen(token));
-                    strncat(strptr, " ", 1);
-
-                }
-
-                printf("\nSoy el hijo y termino de procesar el texto.....\n");
-
-                strncpy(mem, strptr, bits);
-
-                sleep(1);
+                
+                funhijo(mem, membits, cen1);
+                                
                 sem_post(sem_1); /* activa al padre */
-
-                /* libero los semáforos */
-                sem_close(sem_1);
-                sem_close(sem_2);
-                printf("\nSoy el hijo y termino.....\n");
                 break;
 
             default: /*estamos en el padre  */
@@ -148,7 +124,7 @@ int main(int argc, char **argv) {
                 *membits = bits;
 
                 printf("\nSoy el padre termino de leer el stdin .....\n");
-                sleep(1);
+
                 sem_post(sem_2); /* activa al hijo */
         }
 
