883447b4bf0dd4825116ef45588b0030137d46ab
Pablo Martín Anaquín <anaquinpm@gmail.com>
2015-10-06 13:52:40 -0300

    TP N4 funcionando - REVISADO - faltan realizar pequeñas correcciones

---
+++ alumnos/3959/tp4/func.h
@@ -0,0 +1,15 @@
+#ifndef _FUNC_H_
+#define _FUNC_H_
+
+	#define OK_JPEG    "HTTP/1.0 200 OK\nContent-Type:image/jpeg\nContent-Length:%d\n\n"
+	#define OK_HTML    "HTTP/1.0 200 OK\nContent-Type:text/html\nContent-Length:%d\n\n"
+	#define OK_TXT    "HTTP/1.0 200 OK\nContent-Type:text/plain\nContent-Length:%d\n\n"
+	#define OK_PDF     "HTTP/1.0 200 OK\nContent-Type:application/pdf\nContent-Leng:%d\n\n"
+
+	#define NOTOK_404   "HTTP/1.1 404 Not Found\nContent-Type:text/html\n\n"
+	#define MESS_404    "<html><body><h1>FILE NOT FOUND</h1></body></html>"
+	#define NOTOK_500   "HTTP/1.1 500 Internal Server Error\nContent-Type:text/html\n\n" //Cuando muestro el msj?
+	#define MESS_500    "<html><body><h1>Internal Server Error</h1></body></html>"
+	#define HTTP_V	"HTTP/1.0"
+
+#endif
---
+++ alumnos/3959/tp4/main.c
@@ -0,0 +1,64 @@
+#include <unistd.h>
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <netinet/in.h>
+#include <stdlib.h>
+#include <string.h>
+#include <signal.h>
+
+#include "http_worker.h"
+
+int main(int argc, char * const *argv)
+{
+	int sd, sd_conn;
+	int opt = 1;
+//	int  optlen = sizeof(opt); //para setsockopt
+	socklen_t addrlen;
+	struct sockaddr_in srv_addr;
+	struct sockaddr_in cli_addr;
+
+    /* TODO: agregar creación y configuración del socket */
+//	char directorio[BUF_SIZE]={0};
+	if((opt=getopt(argc,argv,"d:"))!=0 && argc==3){
+		strncpy(d_con.ROOT,argv[2],strlen(argv[2]));			
+	}else{	
+		strncpy(d_con.ROOT,"www/",4);			
+	}
+	printf("Mi directorio ROOOT es: %s\n",d_con.ROOT);
+	d_con.PORT=8080;	
+	sd = socket(PF_INET, SOCK_STREAM,0);		 //creo el socket
+	memset (&srv_addr,0,sizeof (struct sockaddr_in));//vacio la  struct
+	srv_addr.sin_family=AF_INET;		
+	srv_addr.sin_port=htons(d_con.PORT);		
+	inet_pton(AF_INET,"127.0.0.1" , &srv_addr.sin_addr.s_addr);
+
+	//bindeo el socket a una direccion
+	if(bind (sd,(struct sockaddr *)&srv_addr, sizeof srv_addr)==-1){
+		perror("bind ()");
+		exit (EXIT_FAILURE);
+	}
+	listen(sd,CONCUR); 		//"n" incoming connections (backlog)
+	signal(SIGCHLD,SIG_IGN);
+	//consumo el stream connection del listen()
+	while( (sd_conn = accept(sd, (struct sockaddr *) &cli_addr, &addrlen)) > 0) {
+		switch (fork()) {
+			case 0: // hijo
+				//http_worker(sd_conn, (struct sockaddr *) &cli_addr);
+				http_worker(sd_conn);
+				return 0;
+
+			case -1: // error
+				break;
+
+			default: // padre
+				break;
+		}
+		close(sd_conn);
+	}
+	close(sd);
+	perror("acept ()");
+	return 0;
+}
+
