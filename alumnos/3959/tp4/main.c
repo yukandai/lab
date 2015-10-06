ea27bf0a44c33d097246ad3751c225b4b5392a28
Pablo Martín Anaquín <anaquinpm@gmail.com>
2015-10-06 20:33:36 -0300

    Minor Fixes

--- alumnos/3959/tp4/main.c
+++ alumnos/3959/tp4/main.c
@@ -12,23 +12,24 @@
 
 int main(int argc, char * const *argv)
 {
-	int sd, sd_conn;
-	int opt = 1;
-//	int  optlen = sizeof(opt); //para setsockopt
+	int sd, sd_conn,opc=0;
+	int opt = 1, optlen = sizeof(opt); 
 	socklen_t addrlen;
 	struct sockaddr_in srv_addr;
 	struct sockaddr_in cli_addr;
 
     /* TODO: agregar creación y configuración del socket */
-//	char directorio[BUF_SIZE]={0};
-	if((opt=getopt(argc,argv,"d:"))!=0 && argc==3){
+	if((opc=getopt(argc,argv,"d:"))!=0 && argc==3){
 		strncpy(d_con.ROOT,argv[2],strlen(argv[2]));			
 	}else{	
 		strncpy(d_con.ROOT,"www/",4);			
 	}
 	printf("Mi directorio ROOOT es: %s\n",d_con.ROOT);
 	d_con.PORT=8080;	
+	
 	sd = socket(PF_INET, SOCK_STREAM,0);		 //creo el socket
+	setsockopt(sd,SOL_SOCKET,SO_REUSEADDR,&opt,optlen); //to avoid "Address already in use"(EADDRINUSE)
+	
 	memset (&srv_addr,0,sizeof (struct sockaddr_in));//vacio la  struct
 	srv_addr.sin_family=AF_INET;		
 	srv_addr.sin_port=htons(d_con.PORT);		
@@ -41,7 +42,7 @@ int main(int argc, char * const *argv)
 	}
 	listen(sd,CONCUR); 		//"n" incoming connections (backlog)
 	signal(SIGCHLD,SIG_IGN);
-	//consumo el stream connection del listen()
+
 	while( (sd_conn = accept(sd, (struct sockaddr *) &cli_addr, &addrlen)) > 0) {
 		switch (fork()) {
 			case 0: // hijo
