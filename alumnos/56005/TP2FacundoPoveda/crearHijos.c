619d6af9b0dbcc6f081d427df742247362d8ec6d
Facundo Poveda <facapoveda@gmail.com>
2016-06-05 17:20:16 -0300

    Mejora en el tp3

--- alumnos/56005/TP2FacundoPoveda/crearHijos.c
+++ alumnos/56005/TP2FacundoPoveda/crearHijos.c
@@ -3,9 +3,8 @@
 /* Función que crea la cantidad de hijos deseada por el usuario*/
 void crearHijos(int cantHijos, int menorMayor, char * archivo, int palabrasTotales){
 	int i;
-	int j;
-	int m=1;
 	int cantBytes;
+    int bytesTexto=0;
 	int fd1[2];
 	int fd2[2];
 	int fd3[2];
@@ -23,8 +22,7 @@ void crearHijos(int cantHijos, int menorMayor, char * archivo, int palabrasTotal
 	float porcentaje;
 	int vectorLetras[15];
 	int vectorParalelo[15];
-	int aux;
-	int aux2;
+    int parte;
 
 	printf("Proceso padre %d \n", getpid());
 	file = open(archivo, O_RDWR);
@@ -46,6 +44,13 @@ void crearHijos(int cantHijos, int menorMayor, char * archivo, int palabrasTotal
 	while((cantBytes = read(file,buffer,BUFFER)) > 0){
 		write(escritura1,buffer,cantBytes);
 	}
+    for (i=0; i<sizeof(buffer); i++) {
+        //Guardo en una variable la cantidad total de bytes del texto
+        bytesTexto++;
+    }
+    //Guardo en una variable la cantidad de bytes que va a procesar cada hijo
+    parte = bytesTexto/cantHijos;
+    printf("Parte: %i %i \n", bytesTexto, parte);
 	cantBytes = 0;
 	close(file);
 
@@ -92,109 +97,7 @@ void crearHijos(int cantHijos, int menorMayor, char * archivo, int palabrasTotal
 					printf("PID hijo monitor: %d - PID Padre: %d \n", getpid(), getppid());
 					//Leemos del fd2 y escribimos en el fd3
 					while((cantBytes = read(lectura2, &buffer, sizeof(char))) > 0){
-						//Limpiamos el vector que vamos a llenar
-						for(i=0;i<15;i++){
-							vectorLetras[i]=0;
-						}
-						//Cargamos el vector paralelo con la cantidad de letras por palabras
-						for(i=0;i<15;i++){
-							vectorParalelo[i]=m;
-							m++;
-						}
-						for(i = 0; i < strlen(buffer); i++){
-							//Contador para palabras con 1 caracter
-							if(buffer[i-1] == ' ' && buffer[i] != ' ' && buffer[i+1] == ' '){
-								vectorLetras[0]++;
-							}
-							//Contador para palabras con 2 caracteres
-							if(buffer[i-1] == ' ' && buffer[i] != ' ' && buffer[i+1] != ' ' && buffer[i+2] == ' '){
-								vectorLetras[1]++;
-							}
-							//Contador para palabras con 3 caracteres
-							if(buffer[i-1] == ' ' && buffer[i] != ' ' && buffer[i+1] != ' ' && buffer[i+2] != ' ' && buffer[i+3] == ' '){
-								vectorLetras[2]++;
-							}
-							//Contador para palabras con 4 caracteres
-							if(buffer[i-1] == ' ' && buffer[i] != ' ' && buffer[i+1] != ' ' && buffer[i+2] != ' ' && buffer[i+3] != ' ' && buffer[i+4] == ' '){
-								vectorLetras[3]++;
-							}
-							//Contador para palabras con 5 caracteres
-							if(buffer[i-1] == ' ' && buffer[i] != ' ' && buffer[i+1] != ' ' && buffer[i+2] != ' ' && buffer[i+3] != ' ' && buffer[i+4] != ' ' && buffer[i+5] == ' '){
-								vectorLetras[4]++;
-							}
-							//Contador para palabras con 6 caracteres
-							if(buffer[i-1] == ' ' && buffer[i] != ' ' && buffer[i+1] != ' ' && buffer[i+2] != ' ' && buffer[i+3] != ' ' && buffer[i+4] != ' ' && buffer[i+5] != ' ' && buffer[i+6] == ' '){
-								vectorLetras[5]++;
-							}
-							//Contador para palabras con 7 caracteres
-							if(buffer[i-1] == ' ' && buffer[i] != ' ' && buffer[i+1] != ' ' && buffer[i+2] != ' ' && buffer[i+3] != ' ' && buffer[i+4] != ' ' && buffer[i+5] != ' ' && buffer[i+6] != ' ' && buffer[i+7] == ' '){
-								vectorLetras[6]++;
-							}
-							//Contador para palabras con 8 caracteres
-							if(buffer[i-1] == ' ' && buffer[i] != ' ' && buffer[i+1] != ' ' && buffer[i+2] != ' ' && buffer[i+3] != ' ' && buffer[i+4] != ' ' && buffer[i+5] != ' ' && buffer[i+6] != ' ' && buffer[i+7] != ' ' && buffer[i+8] == ' '){
-								vectorLetras[7]++;
-							}
-							//Contador para palabras con 9 caracteres
-							if(buffer[i-1] == ' ' && buffer[i] != ' ' && buffer[i+1] != ' ' && buffer[i+2] != ' ' && buffer[i+3] != ' ' && buffer[i+4] != ' ' && buffer[i+5] != ' ' && buffer[i+6] != ' ' && buffer[i+7] != ' ' && buffer[i+8] != ' ' && buffer[i+9] == ' '){
-								vectorLetras[8]++;
-							}
-							//Contador para palabras con 10 caracteres
-							if(buffer[i-1] == ' ' && buffer[i] != ' ' && buffer[i+1] != ' ' && buffer[i+2] != ' ' && buffer[i+3] != ' ' && buffer[i+4] != ' ' && buffer[i+5] != ' ' && buffer[i+6] != ' ' && buffer[i+7] != ' ' && buffer[i+8] != ' ' && buffer[i+9] != ' ' && buffer[i+10] == ' '){
-								vectorLetras[9]++;
-							}
-							//Contador para palabras con 11 caracteres
-							if(buffer[i-1] == ' ' && buffer[i] != ' ' && buffer[i+1] != ' ' && buffer[i+2] != ' ' && buffer[i+3] != ' ' && buffer[i+4] != ' ' && buffer[i+5] != ' ' && buffer[i+6] != ' ' && buffer[i+7] != ' ' && buffer[i+8] != ' ' && buffer[i+9] != ' ' && buffer[i+10] != ' ' && buffer[i+11] == ' '){
-								vectorLetras[10]++;
-							}
-							//Contador para palabras con 12 caracteres
-							if(buffer[i-1] == ' ' && buffer[i] != ' ' && buffer[i+1] != ' ' && buffer[i+2] != ' ' && buffer[i+3] != ' ' && buffer[i+4] != ' ' && buffer[i+5] != ' ' && buffer[i+6] != ' ' && buffer[i+7] != ' ' && buffer[i+8] != ' ' && buffer[i+9] != ' ' && buffer[i+10] != ' ' && buffer[i+11] != ' ' && buffer[i+12] == ' '){
-								vectorLetras[11]++;
-							}
-							//Contador para palabras con 13 caracteres
-							if(buffer[i-1] == ' ' && buffer[i] != ' ' && buffer[i+1] != ' ' && buffer[i+2] != ' ' && buffer[i+3] != ' ' && buffer[i+4] != ' ' && buffer[i+5] != ' ' && buffer[i+6] != ' ' && buffer[i+7] != ' ' && buffer[i+8] != ' ' && buffer[i+9] != ' ' && buffer[i+10] != ' ' && buffer[i+11] != ' ' && buffer[i+12] != ' ' && buffer[i+13] == ' '){
-								vectorLetras[12]++;
-							}
-							//Contador para palabras con 14 caracteres
-							if(buffer[i-1] == ' ' && buffer[i] != ' ' && buffer[i+1] != ' ' && buffer[i+2] != ' ' && buffer[i+3] != ' ' && buffer[i+4] != ' ' && buffer[i+5] != ' ' && buffer[i+6] != ' ' && buffer[i+7] != ' ' && buffer[i+8] != ' ' && buffer[i+9] != ' ' && buffer[i+10] != ' ' && buffer[i+11] != ' ' && buffer[i+12] != ' ' && buffer[i+13] != ' ' && buffer[i+14] == ' '){
-								vectorLetras[13]++;
-							}
-							//Contador para palabras con 15 caracteres
-							if(buffer[i-1] == ' ' && buffer[i] != ' ' && buffer[i+1] != ' ' && buffer[i+2] != ' ' && buffer[i+3] != ' ' && buffer[i+4] != ' ' && buffer[i+5] != ' ' && buffer[i+6] != ' ' && buffer[i+7] != ' ' && buffer[i+8] != ' ' && buffer[i+9] != ' ' && buffer[i+10] != ' ' && buffer[i+11] != ' ' && buffer[i+12] != ' ' && buffer[i+13] != ' ' && buffer[i+14] != ' ' && buffer[i+15] == ' '){
-								vectorLetras[14]++;
-							}
-						}
-						//Si el orden es de menor a mayor
-						if (menorMayor==1){
-							//Ocupamos el método burbuja para ordenar el vector
-							for(i=0; i<15; i++){
-								for(j=0; j<15-i; j++){
-									if(vectorLetras[j] > vectorLetras[j+1]){
-										aux=vectorLetras[j];
-										vectorLetras[j]=vectorLetras[j+1];
-										vectorLetras[j+1]=aux;
-										aux2=vectorParalelo[j];
-										vectorParalelo[j]=vectorParalelo[j+1];
-										vectorParalelo[j+1]=aux2;
-									}
-								}
-							}
-						}
-						//Si el orden es de mayor a menor
-						if (menorMayor==2){
-							for(i=0; i<15; i++){
-								for(j=0; j<15-i; j++){
-									if(vectorLetras[j] < vectorLetras[j+1]){
-										aux=vectorLetras[j];
-										vectorLetras[j]=vectorLetras[j+1];
-										vectorLetras[j+1]=aux;
-										aux2=vectorParalelo[j];
-										vectorParalelo[j]=vectorParalelo[j+1];
-										vectorParalelo[j+1]=aux2;
-									}
-								}
-							}
-
-						}
+						histograma(lectura2,buffer,vectorLetras,vectorParalelo,menorMayor);
 						write (escritura3, vectorLetras, 15*sizeof(int));
 						write (escritura4, vectorParalelo, 15*sizeof(int));
 						break;
