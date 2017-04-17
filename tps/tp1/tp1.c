7eab571f4b51ad074905b64d108ae860b0eb65b1
Cristian Chiappone <cristianchiappone@outlook.com>
2017-04-17 15:26:14 -0300

    Probando subir tp1

--- tps/tp1/tp1.c
+++ tps/tp1/tp1.c
@@ -1,15 +1,24 @@
-/*
- * tp1: Redireccionamiento
- *
- * Federico Hernandez <federico.hernandez@um.edu.ar>
- */
 #include <stdio.h>
 #include <unistd.h>
 #include <string.h>
+#include "contar.h"
 
-
-int main(int argc, char **argv)
-{
-
+int main(int argc, char **argv){
+	int leido,contador=0;
+	char buffer[1500];
+	
+	while ((leido = read(STDIN_FILENO,buffer, sizeof buffer)) > 0){
+		printf("Cantidad de bytes: %d \n",leido);
+		char *delimitador=" \n\t";
+		contador = contar(leido,buffer,delimitador);
+        	printf("Cantidad de palabras: %d \n",contador);
+		delimitador="."; //las oraciones se delimitan por punto
+		contador = contar(leido,buffer,delimitador);
+       		 printf("Cantidad de oraciones: %d \n",contador);
+		delimitador="\303"; //caracter especial
+		contador = contar(leido,buffer,delimitador);
+       		 printf("Cantidad de palabras con c.especiales: %d \n",contador);
+	}
 	return 0;
 }
+
