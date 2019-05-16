d8a31eec5cab061f5a5165bc6a77f88c33fbf99f
Cintia Ossandon <cintia_ossandon@yahoo.com.ar>
2019-05-15 21:25:53 -0300

    arreglos finales tp1

--- alumnos/4140-ossandon-cintia/tps/tp1/funciones.py
+++ alumnos/4140-ossandon-cintia/tps/tp1/funciones.py
@@ -1,14 +1,18 @@
 #!/usr/bin/env python3
-import  os
+
+import os
+
 def abrir_archivo(file):
     '''Abre el archivo indicado en modo lectura'''
     f = open(file, 'r')
     return f
 
-def leer(mq,cant):
+def leer(mq, cant, lock):
+    lock.acquire()
     msg = ""
-    while msg != "chau":
+    while msg != "DONE":
         msg = mq.get()
         palabras = msg.split()
         cant.value += len(palabras)
-        print('proceso: ', os.getpid(), 'contadas: ', len(palabras), 'total: ', cant.value)
+        #print 'proceso: ', os.getpid(), 'contadas: ', len(palabras), 'total: ', cant.value
+    lock.release()
--- alumnos/4140-ossandon-cintia/tps/tp1/tp1.py
+++ alumnos/4140-ossandon-cintia/tps/tp1/tp1.py
@@ -7,8 +7,7 @@ import time
 import getopt
 import multiprocessing
 
-#import funciones
-from funciones import leer, abrir_archivo
+import funciones
 
 def main():
     try:
@@ -26,47 +25,46 @@ def main():
             elif o == "-p":
                 nro = int(a)
             elif o == "-h":
-                print('Ejemplo modo de uso: \n ./tp1.py -f <archivo> -n <tamaño_bloque> -p <cant hijos> \n ./tp1.py -f /etc/services -n 1024 -p 3')
+                print 'Ejemplo modo de uso: \n ./tp1.py -f <archivo> -n <tamaño_bloque> -p <cant hijos> \n ./tp1.py -f /etc/services -n 1024 -p 3'
                 sys.exit(2)
             else:
                 assert False, "option no válida"
 
-        start_index = 0
         procesos = []
 
         mq = multiprocessing.Queue()
+        lock = multiprocessing.Lock()
         cant = multiprocessing.Value('i', 0)
 
         #Creamos n procesos
         for h in range(0, nro):
-            h = multiprocessing.Process(target=leer, args=(mq,cant,))
+            h = multiprocessing.Process(target=funciones.leer, args=(mq,cant,lock,))
             procesos.append(h)
             h.start()
 
-        with abrir_archivo(archivo) as file:
-            file.seek(start_index)
+        with funciones.abrir_archivo(archivo) as file:
             leido = file.read(bloque)
             while leido:
                 mq.put(leido)
-                start_index = start_index + bloque
-                file.seek(start_index)
                 leido = file.read(bloque)
 
         file.close()
+
+        #Le indicamos a los procesos que ya terminamos de poner elementos en la cola
         for i in procesos:
-            mq.put("chau")
+            mq.put("DONE")
 
         #Terminamos los procesos
         for h in procesos:
             h.join()
 
-        print('\npalabras totales: ', cant.value)
+        print '\npalabras totales: ', cant.value
 
     except getopt.GetoptError as err:
         print(err)
         sys.exit(2)
     except OSError:
-        print('el archivo indicado no existe')
+        print 'el archivo indicado no existe'
         sys.exit(2)
 
 if __name__ == "__main__":
