c24ba9c2172c7b9544681f6c58cbacc78da4d3d4
Cintia Ossandon <cintia_ossandon@yahoo.com.ar>
2019-10-01 00:55:17 -0300

    avances tp2

--- alumnos/4140-ossandon-cintia/tps/tp2/funciones.py
+++ alumnos/4140-ossandon-cintia/tps/tp2/funciones.py
@@ -10,32 +10,44 @@ def a_remar():
 def a_bordo():
     print("subido al barco\n")
 
-def hincha_river():
+def hincha_river(lugares_bote, mutex):
     """ Implementar funcion para subir al bote ....si es que se puede ..."""
     print("nuevo hincha....vamos river\n")
+    if len(lugares_bote) > 4:
+        mutex.acquire()
+        lugares_bote.append('HR')
+        a_bordo()
+        mutex.release()
 
-def hincha_boca():
+def hincha_boca(lugares_bote, mutex):
     """ Implementar funcion para subir al bote ....si es que se puede ..."""
     print("nuevo hincha....vamos boca\n")
+    if len(lugares_bote) > 4:
+        mutex.acquire()
+        lugares_bote.append('HB')
+        a_bordo()
+        mutex.release()
 
-def barra_brava_river(hinchas, mutex):
+def barra_brava_river(viajes, lugares_bote, mutex):
     """ Generacion de hinchas de River"""
     while viajes < 20:
         time.sleep(random.randrange(0, 5))
-        hr = threading.Thread(target=funciones.hincha_river)
+        hr = threading.Thread(target=hincha_river, args=(lugares_bote, mutex,))
         hr.start()
-        mutex.acquire()
-        #hincha_river()
-        hinchas.append(hr)
-        mutex.release()
+        # --------------------------------------------------
+        #mutex.acquire()
+        #hinchas.append(hr)
+        #mutex.release()
+        hr.join()
 
-def barra_brava_boca(hinchas, mutex):
+def barra_brava_boca(viajes, lugares_bote, mutex):
     """ Generacion de hinchas de Boca"""
     while viajes < 20:
         time.sleep(random.randrange(0, 5))
-        hb = threading.Thread(target=funciones.hincha_boca)
+        hb = threading.Thread(target=hincha_boca, args=(lugares_bote, mutex,))
         hb.start()
-        mutex.acquire()
-        #hincha_boca()
-        hinchas.append(hb)
-        mutex.release()
+        # --------------------------------------------------
+        #mutex.acquire()
+        #hinchas.append(hb)
+        #mutex.release()
+        hb.join()
--- alumnos/4140-ossandon-cintia/tps/tp2/tp2.py
+++ alumnos/4140-ossandon-cintia/tps/tp2/tp2.py
@@ -11,43 +11,49 @@ import funciones
 
 def main():
     mutex = threading.Lock()
-    hinchas = []
+    #hinchas = []
     lugares_bote = []
     viajes = 0
 
-    t1 = threading.Thread(target=funciones.barra_brava_boca, args=(hinchas, mutex,))
-    t2 = threading.Thread(target=funciones.barra_brava_river, args=(hinchas, mutex,))
+    t1 = threading.Thread(target=funciones.barra_brava_boca, args=(viajes, lugares_bote, mutex,))
+    t2 = threading.Thread(target=funciones.barra_brava_river, args=(viajes, lugares_bote, mutex,))
 
     t1.start()
     t2.start()
 
-    #print("----- INICIO ----- \n")
-    #print("lugares ocupados = %s \n" % (len(lugares_bote)))
-    #print("subidos = %s \n" % lugares_bote)
-    #print("------------------ \n")
-
-    #time.sleep(random.randrange(0, 5))
+    print("----- INICIO ----- \n")
+    print("viajes = %s \n" % (viajes))
+    print("lugares ocupados = %s \n" % (len(lugares_bote)))
+    print("subidos = %s \n" % lugares_bote)
+    print("------------------ \n")
 
     # si estan ocupados todos los lugares en el bote, inicio un viaje y limpio el bote
-    print("viajes = %s \n" % (viajes))
+    # while viajes < 20:
+        #print("chequeando lugares... \n")
+        #if len(lugares_bote) > 4:
+        #    lugares_bote.append(hinchas.pop(0))
+        #    funciones.a_bordo()
+        #elif len(lugares_bote) == 4:
+        #    viajes = viajes + 1
+        #    del lugares_bote[:]
+
     while viajes < 20:
-        print("lugares ocupados = %s \n" % (len(lugares_bote)))
-        if len(lugares_bote) > 4:
-            #print("subidos = %s \n" % lugares_bote)
-            lugares_bote.append(hinchas.pop(0))
-            #funciones.a_bordo()
-            print("bote = %s \n" % (lugares_bote))
-            print("lugares ocupados = %s \n" % (len(lugares_bote)))
-        else:
-            print("bote lleno = %s \n" % (lugares_bote))
+        print("lugares bote = %s \n" % (len(lugares_bote)))
+        if len(lugares_bote) == 4:
+            mutex.acquire()
             viajes = viajes + 1
+            print("viaje realizado con = %s \n" % (lugares_bote))
             del lugares_bote[:]
+            print("bote vacio \n")
+            mutex.release()
 
     t1.join()
     t2.join()
 
+    print("----- FIN ----- \n")
     print("viajes realizados = %s \n" % (viajes))
     print("terminaron los viajes\n")
+    print("------------------ \n")
 
 if __name__ == "__main__":
     main()
