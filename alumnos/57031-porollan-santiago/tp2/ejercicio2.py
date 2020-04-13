b2941dcdfd2d53fcc4b93c8a8f2c60aec2a9325a
Santi Porollan <santiporollan@gmail.com>
2020-04-13 12:11:18 -0300

    ejercicio2

---
+++ alumnos/57031-porollan-santiago/tp2/ejercicio2.py
@@ -0,0 +1,23 @@
+import argparse
+import os
+import math
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='calcular potencia')
+    parser.add_argument('-n', dest='base', type=int, help='ingrese la base')
+    parser.add_argument('-m', dest='expon', type=int, help='ingrese el exponente')
+    parser.add_argument('-v', '--verbose', dest='verb', action='store_const', const=True, default=False, help='ingrese el exponente')
+    args = parser.parse_args()
+    if args.verb:
+        print('argumentos ingresados: (\'-v\', \'\'), (\'-n\', \'',args.base,'\'), (\'-m\', \'',args.expon,'\')')
+        print('PID padre: ', os.getpid())
+        print('Creando hijo')
+    pid = os.fork()
+    if pid == 0:
+        if args.verb:
+            print('PID hijo: ', os.getpid())
+        print('potencia: ', args.base ** args.expon)
+        if args.verb:
+            print('Creado por: ', os.getppid())
+
