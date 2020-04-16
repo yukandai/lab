5c03f1c289fb44cae27a6b7ba7422c71f5d36e6b
Santi Porollan <santiporollan@gmail.com>
2020-04-15 21:05:30 -0300

    corregido

--- alumnos/57031-porollan-santiago/tp2/ejercicio3.py
+++ alumnos/57031-porollan-santiago/tp2/ejercicio3.py
@@ -5,23 +5,24 @@ import math
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='calcular potencia')
-    parser.add_argument('-n', dest='base', type=int, help='ingrese la base')
-    parser.add_argument('-c', dest='cant', type=int, help='cantidad de hijos')
-    parser.add_argument('-v', '--verbose', dest='verb', action='store_const', const=True, default=False, help='ingrese el exponente')
+    parser.add_argument('-n', dest='base', type=int, help='base', required=True)
+    parser.add_argument('-c', dest='cant', type=int, help='cantidad de hijos', default=1)
+    parser.add_argument('-v', '--verbose', dest='verb', action='store_const', const=True, default=False, help='modo verboso')
     args = parser.parse_args()
     if args.verb:
         print('argumentos ingresados: (\'-v\', \'\'), (\'-n\', \'',args.base,'\')')
         print('PID padre: ', os.getpid())
         print('Creando hijos')
     for _ in range(args.cant):
-        expon = int(input('exponente: '))
         pid = os.fork()
         if pid == 0:
+            expon = int(input('\nexponente: '))
             #expon = 2
             if args.verb:
                 print('PID hijo: ', os.getpid())
             print('potencia: ', args.base ** expon)
             if args.verb:
                 print('Creado por: ', os.getppid())
-            break
+            exit()
+        os.wait()
 
