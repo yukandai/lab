8b61cb179c427adb2c5a050cddb10b711df37014
Santi Porollan <santiporollan@gmail.com>
2020-04-15 21:09:30 -0300

    corregido

--- alumnos/57031-porollan-santiago/tp2/ejercicio2.py
+++ alumnos/57031-porollan-santiago/tp2/ejercicio2.py
@@ -5,9 +5,9 @@ import math
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='calcular potencia')
-    parser.add_argument('-n', dest='base', type=int, help='ingrese la base')
-    parser.add_argument('-m', dest='expon', type=int, help='ingrese el exponente')
-    parser.add_argument('-v', '--verbose', dest='verb', action='store_const', const=True, default=False, help='ingrese el exponente')
+    parser.add_argument('-n', dest='base', type=int, help='base', required=True)
+    parser.add_argument('-m', dest='expon', type=int, help='exponente', required=True)
+    parser.add_argument('-v', '--verbose', dest='verb', action='store_const', const=True, default=False, help='modo verboso')
     args = parser.parse_args()
     if args.verb:
         print('argumentos ingresados: (\'-v\', \'\'), (\'-n\', \'',args.base,'\'), (\'-m\', \'',args.expon,'\')')
