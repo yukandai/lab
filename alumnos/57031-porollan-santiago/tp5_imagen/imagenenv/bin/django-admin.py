6e56851a99c50cf92699d1a9024bb730c46553d2
porollansantiago <santiporollan@gmail.com>
2020-05-11 01:14:01 -0300

    probando wand para convertir a p3

---
+++ alumnos/57031-porollan-santiago/tp5_imagen/imagenenv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/santi/univ/compII/lab/alumnos/57031-porollan-santiago/tp5_imagen/imagenenv/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
--- alumnos/57031-porollan-santiago/tp5_imagen/main.py
+++ alumnos/57031-porollan-santiago/tp5_imagen/main.py
@@ -1,6 +1,22 @@
 import argparse
+from wand.image import Image
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description="Procesar imagen")
-    parser.add_argument("")
+    parser.add_argument("-f", dest="archivo", type=str, help="Nombre de la imagen formato ppm", required=True)
+    parser.add_argument("-n", dest="tam", type=int, default=64, help="Tamaño del bloque de bytes")
+    args = parser.parse_args()
+    #with Image(width=100, height=100, pseudo='plasma:') as img: 
+    #    img.save(filename='P6.ppm') 
+
+    # If you specify "compression='no'", you will get P3 (uncompressed ASCII)
+    #with Image(width=100, height=100, pseudo='plasma:') as img: 
+    #    img.compression = 'no' 
+    #    img.save(filename='P3.ppm')
+    f = Image(filename=args.archivo)
+    f.compression = 'no'
+    f.save(filename='new_dow.ppm')
+    with open("new_"+args.archivo, 'w') as newf:
+        for line in f:
+            print(line)
