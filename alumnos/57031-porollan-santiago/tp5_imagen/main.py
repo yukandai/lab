74c02bc7436392fcd67c48d1968cc46045a627c6
porollansantiago <santiporollan@gmail.com>
2020-05-11 17:23:31 -0300

    reescribir imagen

--- alumnos/57031-porollan-santiago/tp5_imagen/main.py
+++ alumnos/57031-porollan-santiago/tp5_imagen/main.py
@@ -1,22 +1,27 @@
 import argparse
-from wand.image import Image
+import os
+import sys
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description="Procesar imagen")
     parser.add_argument("-f", dest="archivo", type=str, help="Nombre de la imagen formato ppm", required=True)
-    parser.add_argument("-n", dest="tam", type=int, default=64, help="Tamaño del bloque de bytes")
+    parser.add_argument("-n", dest="tam", type=int, help="Tamaño del bloque de bytes", default=256)
+    parser.add_argument("-r", dest="red", metavar="RED", nargs=1, required=True, help="Color rojo")
+    parser.add_argument("-g", dest="green", metavar="GREEN", nargs=1, required=True, help="Color verde")
+    parser.add_argument("-b", dest="blue", metavar="BLUE", nargs=1, required=True, help="Color azul")
+    leido = 1
+    with open("newimage.ppm", 'wb') as bi:
+        pass
     args = parser.parse_args()
-    #with Image(width=100, height=100, pseudo='plasma:') as img: 
-    #    img.save(filename='P6.ppm') 
+    fd = os.open(args.archivo, os.O_RDONLY)
 
-    # If you specify "compression='no'", you will get P3 (uncompressed ASCII)
-    #with Image(width=100, height=100, pseudo='plasma:') as img: 
-    #    img.compression = 'no' 
-    #    img.save(filename='P3.ppm')
-    f = Image(filename=args.archivo)
-    f.compression = 'no'
-    f.save(filename='new_dow.ppm')
-    with open("new_"+args.archivo, 'w') as newf:
-        for line in f:
-            print(line)
+    while leido:
+        leido = os.read(fd, args.tam)
+        newleido = []
+        for l in leido:
+            newl = l
+            newleido.append(newl)
+        newimage = bytes(newleido)
+        with open("newimage.ppm", 'ab') as ni:
+            ni.write(newimage)
